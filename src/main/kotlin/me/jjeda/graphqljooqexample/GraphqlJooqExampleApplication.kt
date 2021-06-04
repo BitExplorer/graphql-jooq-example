7a99375210a847b801ec58a4f89f12943e335c77
jjeda <jjeda@naver.com>
2021-06-04 15:49:36 +0900

    feat: init project

---
+++ src/main/kotlin/me/jjeda/graphqljooqexample/GraphqlJooqExampleApplication.kt
@@ -0,0 +1,11 @@
+package me.jjeda.graphqljooqexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphqlJooqExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<GraphqlJooqExampleApplication>(*args)
+}
---
+++ src/test/kotlin/me/jjeda/graphqljooqexample/GraphqlJooqExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.jjeda.graphqljooqexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphqlJooqExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
